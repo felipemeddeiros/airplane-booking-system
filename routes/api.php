b37b9c0e85df82c8329e19655cb95f33592b769a
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2020-12-27 17:42:09 -0300

    Add booking controller to the router

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * Booking management routes
+ */
+Route::prefix('booking')->group(function () {
+
+    Route::get('', [BookingController::class, 'index']);
+    Route::delete('', [BookingController::class, 'destroyAll']);
+    Route::post('/aircrafts/{aircraft}', [BookingController::class, 'store']);
 });
