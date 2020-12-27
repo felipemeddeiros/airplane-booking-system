8259c9ebda2b3c651b74452522cc328a9ee37d18
Felipe Medeiros <felipe.meddeiros@hotmail.com>
2020-12-27 17:39:06 -0300

    Add relationship with bookings

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Passenger extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function bookings()
+    {
+        return $this->hasMany('App\Models\Booking')->where('canceled', 0);
+    }
 }
